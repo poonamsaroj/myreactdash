5ec7976d0c3795da2e7736b1e73ac56c0168cf1a
Saroj <poonam.saroj@capgemini.com>
2018-11-21 16:33:54 +0530

    Second commit

--- src/components/navbarpage/Navbarpage.js
+++ src/components/navbarpage/Navbarpage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import './Navbarpage.css';
 
 const styles = {
   root: {
@@ -25,17 +27,19 @@ function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+        <Grid container spacing={40}>
+            <AppBar position="static" className="appbar">
+                <Toolbar>
+                {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton> */}
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                    Food Exchange
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Grid>
     </div>
   );
 }
